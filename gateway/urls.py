83f0494a8c48dfa92a02e9fc0c893a615ee11679
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2022-12-14 23:59:41 +0300

    refresh token view

--- gateway/urls.py
+++ gateway/urls.py
@@ -5,5 +5,6 @@ print("burasi urls")
 urlpatterns = [
     path('login',views.LoginView.as_view()),
     path('register',views.RegisterView.as_view()),
+    path('refresh',views.RefreshView.as_view()),
 
 ]
